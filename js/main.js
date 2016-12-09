9a1a8a7226663648eda63261e4e91663dc6b665e
aheitzen <alexaheitzenrader@yahoo.com>
2016-12-09 13:56:24 -0800

    fixed halg responsiveness

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-var myArray = ['aspiring aerialist', 'feminist', 'Sass lover', 'Seattleite', 'graphic design enthusiast', 'cat person', 'Bootstrap wizard'];
+var myArray = ['aspiring aerialist', 'feminist', 'Sass specialist', 'Slytherin', 'Seattleite', 'graphic design enthusiast', 'cat person', 'Bootstrap wizard', 'Attack on Titan fangirl', 'logo designer'];
 var currentIndex = 0
 var timer = setInterval(function(){
-	$("#selector").text('& ' + myArray[currentIndex]);
+	$("#selector").text('& ' + myArray[currentIndex] + ' .');
 	currentIndex++;
 	if (currentIndex == myArray.length) {
 		currentIndex = 0;
