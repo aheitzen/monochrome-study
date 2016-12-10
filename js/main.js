3ad53de3266058abe990be43cb2ba96648e10161
aheitzen <alexaheitzenrader@yahoo.com>
2016-12-09 21:20:46 -0800

    finsihed email and partial done with last block element

--- js/main.js
+++ js/main.js
@@ -12,6 +12,14 @@ $(document).ready(function () {
 	$("div#pink-box").mouseover(function(){
 		$(this).html('Full Name: Alexa Heitzenrader<br/>Skillset: HTML5/CSS3/SASS & JavaScript/jQuery & Bootstrap/Photoshop<br/>');
 	}).mouseout(function() {
-		$(this).html('<p id="about">about</p>');
+		$(this).html('<p class="about">about</p>');
+	});
+});
+
+$(document).ready(function () {
+	$("#full-orange").mouseover(function(){
+		$(this).html('<i class="fa fa-github" aria-hidden="true"></i><br/><i class="fa fa-linkedin-square" aria-hidden="true"></i><br/><i class="fa fa-envelope-o" aria-hidden="true"></i>');
+	}).mouseout(function() {
+		$(this).html('<p id="info">stalk me</p>');
 	});
 });
