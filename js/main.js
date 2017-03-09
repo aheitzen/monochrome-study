6485af12718e3d6d6a6aa36058d5f97767f710f1
aheitzen <alexaheitzenrader@yahoo.com>
2017-03-08 19:17:45 -0800

    super sketch css

--- js/main.js
+++ js/main.js
@@ -8,21 +8,21 @@ var timer = setInterval(function(){
 	}
  }, 5000);
 
-$(document).ready(function () {
-	$("div#pink-box").mouseover(function(){
-		$(this).html('<p>Hi Friends!</p><br/><p id="second-about">Welcome to my portfolio where you can view my past and current projects. I specialize in front-end development with a focus on responsive design and user interface. When I am not coding you can find me reading or running off to join the circus.</p>');
-	}).mouseout(function() {
-		$(this).html('<p class="about">about</p>');
-	});
-});
+// $(document).ready(function () {
+// 	$("div#pink-box").mouseover(function(){
+// 		$(this).html('<p>Hi Friends!</p><br/><p id="second-about">Welcome to my portfolio where you can view my past and current projects. I specialize in front-end development with a focus on responsive design and user interface. When I am not coding you can find me reading or running off to join the circus.</p>');
+// 	}).mouseout(function() {
+// 		$(this).html('<p class="about">about</p>');
+// 	});
+// });
 
-$(document).ready(function () {
-	$("#full-orange").mouseover(function(){
-		$(this).html('<i class="fa fa-github" aria-hidden="true"></i><br/><i class="fa fa-linkedin-square" aria-hidden="true"></i><br/><i class="fa fa-envelope-o" aria-hidden="true"></i>');
-	}).mouseout(function() {
-		$(this).html('<p id="info">stalk me</p>');
-	});
-});
+// $(document).ready(function () {
+// 	$("#full-orange").mouseover(function(){
+// 		$(this).html('<i class="fa fa-github" aria-hidden="true"></i><br/><i class="fa fa-linkedin-square" aria-hidden="true"></i><br/><i class="fa fa-envelope-o" aria-hidden="true"></i>');
+// 	}).mouseout(function() {
+// 		$(this).html('<p id="info">stalk me</p>');
+// 	});
+// });
 $(document).ready(function() {
 	$(".card").flip({
   	axis: 'y',
